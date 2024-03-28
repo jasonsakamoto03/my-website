600145a8b35e982b074a5519ae1fdd60950d0a9c
Jason Sakamoto <jasons03@uw.edu>
2024-03-27 23:16:47 -0700

    ready to deploy

--- src/router.js
+++ src/router.js
@@ -3,12 +3,10 @@ import App from "./App";
 import Contact from "./Routes/Contact";
 import About from "./Routes/About";
 import Projects from "./Routes/Projects";
-import Blog from "./Routes/Blog";
 
 export const router = createBrowserRouter([
     {path: "/", element: <App />},
     {path: "/about", element: <About />},
     {path: "/projects", element: <Projects />},
-    {path: "/blog", element: <Blog />},
     {path: "/contact", element: <Contact />}
   ]);
